c236f6500d8ec977f443e53563adb0d0433a9906
echilov.gamzat <echilov.gamzat@wb.ru>
2025-03-24 17:51:20 +0300

    config struct

---
+++ internal/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env        string     `yaml:"env" env:"YAKVS_ENV" env-default:"local"`
+	HttpServer HTTPServer `yaml:"http_server" env:"YAKVS_HTTP_SERVER"`
+	Tarantool  Tarantool  `yaml:"tarantool" env:"YAKVS_TARANTOOL"`
+}
+
+type HTTPServer struct {
+	Address        string        `yaml:"address" env-default:"localhost:8080"`
+	ReadTimeout    time.Duration `yaml:"read_timeout" env-default:"4s"`
+	IdleTimeout    time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	ContextTimeout time.Duration `yaml:"context_timeout" env-default:"10s"`
+	WriteTimeout   time.Duration `yaml:"write_timeout" env-default:"60s"`
+}
+
+type Tarantool struct {
+	Host     string `yaml:"host"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+}
+
+func ParseConfig() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	cfg := &Config{}
+
+	if err := cleanenv.ReadConfig(configPath, cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return cfg
+}
